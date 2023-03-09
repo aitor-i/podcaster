bcdab655734d924e3b6b2882b09fe9a1e9d99e04
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-09 19:06:38 +0100

    ADD - HomePage

--- podcaster-web/src/index.tsx
+++ podcaster-web/src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom';
+
 import App from 'service/ui/App';
+import HomePage from 'service/ui/layout/HomePage';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);

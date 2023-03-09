a6337a4deedcd9bc53bc30b40eb9f505ec9244d3
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-09 19:07:26 +0100

    DEL - App component

--- podcaster-web/src/service/ui/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from 'service/ui/App';
-
-describe('App', () => {
-  it('should render App component', () => {
-    render(<App />);
-  });
-});

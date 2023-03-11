5f63175df63c9c31b98ac1d85e86fa125e1a7218
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-11 14:20:26 +0100

    ADD - Podcast detail body

---
+++ podcaster-web/src/service/ui/components/PodcastDetailBody/PodcastDetailBody.tsx
@@ -0,0 +1,29 @@
+import { useMemo } from 'react';
+
+import { useParams } from 'react-router-dom';
+
+import { useFetch } from 'service/ui/hooks/useFetch';
+
+import PodcastInformationCard from 'service/ui/components/PodcastInformationCard';
+
+import PodcastDetailBodyStyles from './PodcastDetailBody.module.css';
+
+import type { IFetcher } from 'service/fetcher/IFetcher';
+import type { RootObject as IDetailData } from 'domain/model/IPodcastDetailData';
+import PodcastEpisodes from './PodcastEpisodes';
+
+export const PodcastDetailBody = () => {
+  const { podcastId } = useParams();
+  const fetchPodcastDetailConfig: IFetcher = useMemo(
+    () => ({ method: 'GET', url: `https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=20` }),
+    [podcastId]
+  );
+  const { apiData, fetchingState } = useFetch<IDetailData>({ fetchConfig: fetchPodcastDetailConfig });
+
+  return (
+    <div className={PodcastDetailBodyStyles.container}>
+      <PodcastInformationCard />
+      {fetchingState === 'success' && <PodcastEpisodes episodeCount={apiData?.resultCount!} episodeList={apiData?.results!} />}
+    </div>
+  );
+};
---
+++ podcaster-web/src/service/ui/components/PodcastDetailBody/index.ts
@@ -0,0 +1,3 @@
+import { PodcastDetailBody } from './PodcastDetailBody';
+
+export default PodcastDetailBody;

ee30ed93fbe80b71e4e9c00a94ebf4c5897a3468
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-11 11:20:47 +0100

    UPDATE - Home page styles

--- podcaster-web/src/service/ui/layout/HomePage/HomePage.tsx
+++ podcaster-web/src/service/ui/layout/HomePage/HomePage.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import Header from 'service/ui/components/Header';
+import PodcastGrid from 'service/ui/components/PodcastGrid';
+
+import HomePageStyles from './HomePage.module.css';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={HomePageStyles.homePage}>
+      <Header />
+      <PodcastGrid />
+    </div>
+  );
 };

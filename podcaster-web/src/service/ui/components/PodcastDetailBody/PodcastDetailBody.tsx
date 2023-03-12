225ed98b9457bf79564fe551bbe8af7326e75365
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-12 17:43:45 +0100

    ADD - 24h data fetching detail

--- podcaster-web/src/service/ui/components/PodcastDetailBody/PodcastDetailBody.tsx
+++ podcaster-web/src/service/ui/components/PodcastDetailBody/PodcastDetailBody.tsx
@@ -1,24 +1,47 @@
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 
 import { useParams } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+
+import { podcastsDetailState as podcastsDetailAtom } from 'domain/atoms/podcastDetails';
 
 import { useFetch } from 'service/ui/hooks/useFetch';
 
 import PodcastInformationCard from 'service/ui/components/PodcastInformationCard';
 
 import PodcastDetailBodyStyles from './PodcastDetailBody.module.css';
+import PodcastEpisodes from './PodcastEpisodes';
 
 import type { IFetcher } from 'service/fetcher/IFetcher';
 import type { RootObject as IDetailData } from 'domain/model/IPodcastDetailData';
-import PodcastEpisodes from './PodcastEpisodes';
+import type { IPodcastDetailsState } from 'domain/atoms/podcastDetails';
 
 export const PodcastDetailBody = () => {
+  const [podcastsDetailState, setPodcastDetailState] = useRecoilState(podcastsDetailAtom);
   const { podcastId } = useParams();
   const fetchPodcastDetailConfig: IFetcher = useMemo(
     () => ({ method: 'GET', url: `https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=20` }),
     [podcastId]
   );
-  const { apiData, fetchingState } = useFetch<IDetailData>({ fetchConfig: fetchPodcastDetailConfig });
+  const detailFromStore = podcastsDetailState.find(detail => detail.podcastDetail.results[0].collectionId === Number(podcastId));
+  const detailFetchingDate = podcastsDetailState.find(detail => detail.podcastDetail.results[0].collectionId === Number(podcastId))?.updateDate;
+
+  const { apiData, fetchingState, isUpdated } = useFetch<IDetailData>({ fetchConfig: fetchPodcastDetailConfig, fetchDate: detailFetchingDate });
+
+  const updateDetailHandler = useCallback(() => {
+    const detailsWithoutCurrent = podcastsDetailState.filter(detail => detail.podcastDetail.results[0].collectionId !== Number(podcastId));
+    const newDate = new Date();
+    const updatedDetail: IPodcastDetailsState = { podcastDetail: apiData!, updateDate: newDate };
+    const updatedDetails: IPodcastDetailsState[] = [...detailsWithoutCurrent, updatedDetail];
+
+    setPodcastDetailState(updatedDetails);
+  }, [apiData, podcastId, podcastsDetailState, setPodcastDetailState]);
+
+  useEffect(() => {
+    if (isUpdated && fetchingState === 'success') {
+      updateDetailHandler();
+    }
+  }, [fetchingState, isUpdated, updateDetailHandler]);
 
   return (
     <div className={PodcastDetailBodyStyles.container}>
@@ -26,7 +49,9 @@ export const PodcastDetailBody = () => {
         <PodcastInformationCard />
       </div>
       <div className={PodcastDetailBodyStyles.episodesContainer}>
-        {fetchingState === 'success' && <PodcastEpisodes episodeCount={apiData?.resultCount!} episodeList={apiData?.results!} />}
+        {fetchingState !== 'loading' && (
+          <PodcastEpisodes episodeCount={detailFromStore?.podcastDetail.resultCount!} episodeList={detailFromStore?.podcastDetail.results!} />
+        )}
       </div>
     </div>
   );

13e94117ebb97f557b71fe85a7874424b80bafa8
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-12 16:50:25 +0100

    ADD - Episode detail route

--- podcaster-web/src/App.tsx
+++ podcaster-web/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import HomePage from 'service/ui/layout/HomePage';
 import PodcastDetail from 'service/ui/layout/PodcastDetail';
+import EpisodeDetail from './service/ui/layout/EpisodeDetail';
 
 export const App = () => {
   return (
@@ -13,6 +12,7 @@ export const App = () => {
         <Routes>
           <Route element={<HomePage />} path="/" />
           <Route element={<PodcastDetail />} path="/podcast/:podcastId" />
+          <Route element={<EpisodeDetail />} path="/podcast/:podcastId/episode/:episodeId" />
         </Routes>
       </BrowserRouter>
     </RecoilRoot>

790256683946ef97e4558356ac62117c06d6f3ba
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-12 20:33:11 +0100

    ADD - PodcastInformationCard on Episode detail

--- podcaster-web/src/service/ui/components/EpisodeDetailBody/EpisodeDetailBody.tsx
+++ podcaster-web/src/service/ui/components/EpisodeDetailBody/EpisodeDetailBody.tsx
@@ -2,20 +2,38 @@ import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import { podcastsDetailState as podcastDetailAtom } from 'domain/atoms/podcastDetails';
+import { podcastState as podcastsStateAtom } from 'domain/atoms/podcasts';
+
+import PodcastInformationCard from 'service/ui/components/PodcastInformationCard';
+
 import EpisodeContainer from './EpisodeContainer';
+
+import EpisodeBodyStyles from './EpisodeDetailBody.module.css';
 
 export const EpisodeDetailBody = () => {
   const podcastsDetailState = useRecoilValue(podcastDetailAtom);
-  const { episodeId } = useParams();
+  const podcastState = useRecoilValue(podcastsStateAtom);
+
+  const { episodeId, podcastId } = useParams();
 
   const episodeDetail = podcastsDetailState.find(detail => detail.podcastDetail.results.find(episode => episode.trackId === Number(episodeId)));
   const episode = episodeDetail?.podcastDetail.results.find(episode => episode.trackId === Number(episodeId));
 
-  console.log('episode', episode);
-  console.log('episodeDetail', episodeDetail);
+  const podcast = podcastState.podcast.find(podcast => podcast.id.attributes['im:id'] === podcastId);
+
   return (
-    <div>
-      <div>Information</div>
+    <div className={EpisodeBodyStyles.container}>
+      <div>
+        {podcast && (
+          <PodcastInformationCard
+            author={podcast?.['im:artist'].label}
+            description={podcast?.summary.label}
+            id={podcast.id.attributes['im:id']}
+            imageSrc={podcast?.['im:image'][2].label}
+            podcastName={podcast?.title.label}
+          />
+        )}
+      </div>
       <EpisodeContainer descriptions={episode?.description!} episodeUrl={episode?.episodeUrl!} title={episode?.trackName!} />
     </div>
   );

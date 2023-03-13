18d05abc370c24f33e28104dc2899c614efda1b1
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-03-13 20:38:05 +0100

    FIX -fetcher cors

--- podcaster-web/src/service/fetcher/fetcher.ts
+++ podcaster-web/src/service/fetcher/fetcher.ts
@@ -1,13 +1,17 @@
 import type { IFetcher } from './IFetcher';
 
 export const fetcher = async ({ url, method, headers, mode, body }: IFetcher) => {
-  const response = await fetch(url, {
+  // const allowedUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
+  // Oops... Request Timeout.
+  const config = {
     method: method,
     headers: headers,
     mode: mode,
     body: body
-  });
+  };
+  const response = await fetch(url, config);
 
   if (!response.ok) throw new Error(response.toString());
+
   return await await response.json();
 };
